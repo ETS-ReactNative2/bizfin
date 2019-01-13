f57eb321050ab8950c61efc0418c01e9f966a323
pedro azpurua <ppazpurua@gmail.com>
2019-01-13 09:21:22 -0400

    ppa

--- src/components/bizaccount.js
+++ src/components/bizaccount.js
@@ -3,6 +3,8 @@ import { Navbar,NavbarBrand,Media,Card,CardImg,CardImgOverlay,CardText,CardBody,
 
 import MapBiz from '../components/map';
 import Footer from '../components/footer';
+//https://blackrockdigital.github.io/startbootstrap-modern-business/
+//https://codepen.io/alexdevero/pen/JYpMEO
 class BizAccount extends React.Component {
   constructor(props) {
     super(props);
@@ -29,15 +31,17 @@ class BizAccount extends React.Component {
   });
     return (
       <div>
-      <div  className="col-12 col-md-5 m-1">
-      <Card tag="li">
-       
-            <CardImg width="100%" src={'assets/images/biz1.png'} alt={producto.name} />
+        
+      <div  className="col-12">
+      <Card >       
+            <CardImg width="60%" src={'http://www.asocem.org.pe/archivo/files/shutterstock_244921597.jpg'} alt={'producto.name'} />
             <CardImgOverlay>
                  <CardTitle>{"producto.name"}</CardTitle>
             </CardImgOverlay>
         
       </Card>
+      
+     
     </div>
       <div className="container">
             <div className="row">              
